f46a9fb7ec035e0b6baea8efe0b5684d926c2482
Richard Slater <git@richard-slater.co.uk>
2012-08-31 23:35:40 +0100

    Tidy Up

--- NitriqTeamCity.Tests/WhenTestingXDocumentWriter.cs
+++ NitriqTeamCity.Tests/WhenTestingXDocumentWriter.cs
@@ -5,14 +5,16 @@ using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using NUnit.Framework;
+using NitriqTeamCity.ComponentModel;
+using NitriqTeamCity.IO;
 
 namespace NitriqTeamCity.Tests {
     [TestFixture]
     public class WhenTestingXDocumentWriter {
         [Test]
         public void ShouldCallStreamWriter() {
-            var streamWriterMock = new Mock<NitriqTeamCity.ComponentModel.IStreamWriter>();
-            var docWriter = new NitriqTeamCity.IO.XmlDocumentWriter(streamWriterMock.Object);
+            var streamWriterMock = new Mock<IStreamWriter>();
+            var docWriter = new XmlDocumentWriter(streamWriterMock.Object);
             docWriter.WriteXDocument(new XDocument(new XElement("test")), "test-file");
             streamWriterMock.Verify(m => m.WriteStream(It.IsAny<Stream>(), "test-file"), Times.Once());
         }
