ec7a6e2dd4173c2e32c9828d46a1f1ce97a41c62
Richard Slater <git@richard-slater.co.uk>
2012-09-01 11:00:15 +0100

    Added --format tag to command line runner
    --format tag is unused, but required at present planning to add support
    for a non-TeamCity specific format for use in a MSBuild task.

--- NitriqTeamCity.Console/Program.cs
+++ NitriqTeamCity.Console/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 
 namespace NitriqTeamCity.Console {
     class Program {
+        private static Action<int> _exitAction;
+
+        public static Action<int> ExitAction {
+            get {
+                if (_exitAction == null) {
+                    _exitAction = (exitCode) => Environment.Exit(exitCode);
+                }
+                return _exitAction;
+            }
+            set { _exitAction = value; }
+        }
+
         static void Main(string[] args) {
             Header();
 
@@ -11,16 +23,19 @@ namespace NitriqTeamCity.Console {
                 Usage();
             }
 
-            var input = ExtractArgument("-input", args);
-            var output = ExtractArgument("-output", args);
+            var format = ExtractArgument("--format", "-f", args);
+            var input = ExtractArgument("--input", "-i", args);
+            var output = ExtractArgument("--output", "-o", args);
 
-            W("Input File: " + input.Trim('"'));
-            W("Output File: " + output.Trim('"'));
+            W("Format: {0}", format);
+            W("Input File: {0}", input.Trim('"'));
+            W("Output File: {0}", output.Trim('"'));
+
             NewLine();
 
             StaticParser.Execute(input, output);
 
-            Exit("Done.");
+            Exit("Done.", 0);
         }
 
         private static void Header() {
@@ -30,36 +45,51 @@ namespace NitriqTeamCity.Console {
         }
 
         private static void Usage() {
-            W("Usage:");
-            W("  NitriqTeamCity.Console.exe -input:<input.html> -output:<teamcity-info.xml>");
+            W("Usage");
+            W("=====");
+            NewLine();
+            W("Long Format:");
+            W("  NitriqTeamCity.Console.exe --format:TeamCityInfoXml --input:<input.html> --output:<teamcity-info.xml>");
+            NewLine();
+            W("Short Format:");
+            W("  NitriqTeamCity.Console.exe -f:TeamCityInfoXml -i:<input.html> -o:<teamcity-info.xml>");
             NewLine();
         }
 
-        private static string ExtractArgument(string argument, params string[] args) {
+        private static string ExtractArgument(string argument, string shortArgument, params string[] args) {
             args = args.Select(a => a.Trim()).ToArray();
 
-            if (args.Count(a => a.StartsWith(argument)) != 1) {
+            Func<string, bool> argSelector = a => a.StartsWith(argument) || a.StartsWith(shortArgument);
+            
+            var argCount = args.Count(argSelector);
+
+            if (argCount < 1) {
                 Usage();
-                Exit(argument + " not specified correctly.", 1);
+                Exit("{0} not specified", 1, argument);
             }
 
-            var value = args.Single(a => a.StartsWith(argument));
+            if (argCount > 1) {
+                Usage();
+                Exit("more than one {0} specified, expected only 1.", 1, argument);
+            }
+
+            var value = args.Single(argSelector);
 
             if (!value.Contains(":")) {
                 Usage();
-                Exit(String.Format("  ERROR: {0} is malformed.", argument));
+                Exit("{0} is malformed.", 1, argument);
             }
 
             return value.Remove(0, value.IndexOf(":") + 1);
         }
 
-        private static void Exit(string value, int exitCode = 0) {
-            W(value);
-            Environment.Exit(exitCode);
+        private static void Exit(string format, int exitCode, params object[] args) {
+            W(format, args);
+            ExitAction(exitCode);
         }
 
-        private static void W(string value){
-            System.Console.WriteLine(value);
+        private static void W(string format, params object[] args) {
+            System.Console.WriteLine(format, args);
         }
 
         private static void NewLine() {
